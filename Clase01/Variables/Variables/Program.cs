3bcd30a42e0225a424c0c2f1d534b75fc66b1973
Andrés Villa <robertoavilla@gmail.com>
2020-10-08 10:48:46 -0300

    Clase 1

---
+++ Clase01/Variables/Variables/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre; // Cadena de caracteres, "texto"
+            nombre = "Andres"; // Asignacion
+
+            Console.Write("Ingrese su nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Su nombre es {0}", nombre);
+
+            const double PI = 3.1415926; // Esto es una constante
+            double radio = 4; // Esto es una variable
+
+            Console.WriteLine("El perimetro de una circunferencia de radio {0} es {1}", radio, 2 * PI * radio);
+
+
+            int hoy = 2020;
+            int nacimiento = 1998;
+
+            int edad = hoy - nacimiento;
+
+            Console.WriteLine("La edad es {0}", edad);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+# Curso Introducción a la Programación con CSharp
+#### Educación It
+#### Instructor: Andrés Villa
+
+En este repositorio encontrarán todo el código que vayamos haciendo en clase para que si no llegan a copiar lo tengan disponible.  
+Además, voy a dejar toda información relevante al curso
+
+### Instaladores
+
+[Visual Studio](https://visualstudio.microsoft.com/es/ "Visual Studio")  
+
+### Otros vínculos
+[Educacion IT](http://www.educacionit.com "Educacion IT")  
+[Alumni](http://alumni.educacionit.com "Alumni")  
